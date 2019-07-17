c23b1883acb1bf692e65137777adec3ed75ffebb
Jimmy Lin <jimmy.lin1313@gmail.com>
2019-07-16 18:17:38 -0700

    test multicore run on levi

--- lib/parallel_osdt.py
+++ lib/parallel_osdt.py
@@ -196,7 +196,7 @@ class ParallelOSDT:
             task = self.dequeue() # Change to non-blocking since we don't have an alternatve idle task anywyas
             if task == None:
                 if self.verbose or self.log:
-                    self.print("Worker {} Idle".format(self.worker_id))
+                    self.print("Worker {} Idle {}".format(self.worker_id, self.terminate()))
                 # results.__refresh__()
                 continue
             (priority, capture, path) = task
